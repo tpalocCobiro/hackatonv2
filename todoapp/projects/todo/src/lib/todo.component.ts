d5a6828787d6108684b1df8b96786a0f84ad02a4
tpalocCobiro <tomasz.paloc@cobiro.com>
2022-04-08 12:51:27 +0200

    added todo lib

---
+++ todoapp/projects/todo/src/lib/todo.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-todo',
+  template: `
+    <p>
+      todo works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ todoapp/projects/todo/src/lib/todo.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { TodoComponent } from './todo.component';
+
+
+
+@NgModule({
+  declarations: [
+    TodoComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    TodoComponent
+  ]
+})
+export class TodoModule { }
